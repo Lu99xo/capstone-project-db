3021d3dcbb6a17316b3de0b5c990fa3c4c12b45e
lu99xo <b4u.dreamer@gmail.com>
2023-11-29 21:12:54 +0300

    add values

---
+++ Insert_bookings.sql
@@ -0,0 +1,6 @@
+INSERT INTO booking(bookingID, tableID, bookingDate, customerID_BK)
+VALUES 
+	( 1 , 5 , "2022-10-10", 1 ),
+    ( 2 , 3 , "2022-11-12", 3 ),
+    ( 3 , 2 , "2022-10-11", 2 ) ,
+    ( 4 , 2 , "2022-10-13", 1 ) ;
