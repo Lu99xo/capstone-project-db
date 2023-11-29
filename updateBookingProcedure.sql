683c83ca91027cf14cda11f4d5ea3af97d2f60a6
Lu99xo <77988610+Lu99xo@users.noreply.github.com>
2023-11-29 22:06:21 +0300

    Delete updateBookingProcedure.sql

--- updateBookingProcedure.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE CancelBooking(IN bookingID INT ) 
-	BEGIN 
-		UPDATE booking
-		SET bookingID = bookingID,
-     		bookingDate = bookingDate	
-	END //
-DELIMITER;
-CALL CancelBooking(9);
