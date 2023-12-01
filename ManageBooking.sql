143b5f515ec34b336391cd58973f6b6f1b7c59f4
lu99xo <b4u.dreamer@gmail.com>
2023-12-01 15:45:29 +0300

    update if statment

--- ManageBooking.sql
+++ ManageBooking.sql
@@ -1,12 +1,14 @@
 DELIMITER //
 CREATE PROCEDURE CheckBooking(IN bookingDate DATE , IN tableID INT ) 
 	BEGIN 
-		SELECT tableID FROM booking
+		SELECT bokingID FROM booking
         WHERE
-		booking.cusotmerID_BK != NULL
-        AND booking.bookingDate = bookingDate 
-        AND booking.tableID = tableID ;
+        booking.bookingDate = bookingDate AND booking.tableID = tableID 
+        
+        IF booking.cusotmerID_BK IS NOT NULL
         SELECT " IS ALREADY BOOKED " 
+        ELSE SELECT CONCAT('Table ', tableID , ' is still aviable')
+        END IF
 	END //
 DELIMITER;
 CALL CheckBooking("2022-10-11" , 2);
